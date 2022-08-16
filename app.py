6a5190be6cf5109da3859954f1b0146d6b5d42db
chief-codes <me.chief@proton.me>
2022-08-16 19:31:33 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,77 +1,14 @@
 import sqlite3
 from flask import Flask, url_for, redirect, session, render_template, request
+from flask_bcrypt import Bcrypt
 import random, string
+from db_def import register_members_to_db, check_members
+#import Password_Function
 
-def register_members_to_db(name,password):
-        con = sqlite3.connect("database.db")
-        cur = con.cursor()
-        cur.execute("INSERT INTO members(name, password) values(?,?)", (name,password))
-        con.commit()
-        con.close()
-
-def check_members(name,password):
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
-    cur.execute("Select name, password FROM members WHERE name=? and password=?", (name,password))
-
-    result = cur.fetchone()
-    if result:
-        return True
-    else:
-        return False
+bcrypt = Bcrypt()
 
 app = Flask(__name__)
 app.secret_key = "wh1t_2_Do?"
-
-#For password to be accepted it must meet the following requirements:
-    #Not be less than 12 characters
-    #Must contain at least 1 number
-    #Must contain both lower and uppercase letters
-    #Must contain special characters or punctuation
-
-lower = string.ascii_lowercase # saves lowercase alphabets to lower
-upper = string.ascii_uppercase # saves uppercase alphabets to upper
-numbers = string.digits # saves numbers to numbers
-punctuation = string.punctuation
-
-def password_generator():
-        all = lower + upper + numbers #+ characters.... combines lower, upper and numbers to form a list
-        temp = random.sample(all, 12) # selects randomly 8 characters anf saves them as temp
-        separator = ""
-        password = separator.join(temp) 
-        #print(password)
-
-def password_validation(password):
-    l,u,n,p = 0,0,0,0
-    if len(password) >= 12:
-        for char in password:
-            if (char in lower):
-                l+=1
-                    
-            if (char in upper):
-                u+=1
-                    
-            if (char in numbers): 
-                n+=1
-                
-            if (char in punctuation):
-                p+=1
-                
-            if (l>=1 & u>=1 & n>=1 & p>=1):
-                print("Password is VALID !!!!")
-        
-            elif l<1:
-                print("Password must contain at least one lower case")
-            elif u<1:
-                print("Password must contain at least one upper case")
-            elif n<1:
-                print("Password must contain at least one number")
-            elif p<1:
-                print("Password must contain at least one punctuation")
-
-            else:
-                print("Password should be 12 or more characters!!!")
- 
 
 @app.route("/")
 def index():
@@ -83,7 +20,10 @@ def register():
         name = request.form["name"]
         password = request.form["password"]
 
-        register_members_to_db(name,password)
+        hash_name = bcrypt.generate_password_hash(name)
+        hash_password = bcrypt.generate_password_hash(password)
+
+        register_members_to_db(hash_name, hash_password)
         return redirect(url_for("index"))
 
     else:
@@ -95,8 +35,14 @@ def login():
         name = request.form["name"]
         password = request.form["password"]
 
-        if check_members(name,password):
-            session["name"] = name
+        hash_name = bcrypt.generate_password_hash(name)
+        hash_password = bcrypt.generate_password_hash(password)
+
+        #verify_hash_name = bcrypt.check_password_hash(hash_name, name )
+        #verify_hash_password = bcrypt.check_password_hash(hash_password, password)
+
+        if check_members(hash_name, hash_password):
+            session["name"] = hash_name
 
         return redirect(url_for("home"))
     else:
@@ -105,7 +51,7 @@ def login():
 @app.route("/home", methods=["POST", "GET"])
 def home():
     if "name" in session:
-        return render_template("home.html", name=session["name"])
+        return render_template("home.html", hash_name=session["name"])
     else:
         return "Name or Pasword is Incorrect"
 
